732f219bb2bc47f3c9808367ee77ea0ff938c0e9
Preston Bernstein <prestonbernstein@gmail.com>
2014-04-13 15:57:15 -0400

    working bootstrap in class_9

--- class_9/website/server.rb
+++ class_9/website/server.rb
@@ -3,3 +3,11 @@ require "sinatra"
 get '/' do
 	erb :index, layout: :index
 end
+
+get '/blogpost' do
+  erb :blogpost, layout: :blogpost
+end
+
+get '/newpost' do
+  erb :newpost, layout: :newpost
+end
