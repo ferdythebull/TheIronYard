3bf5b49400b82c333139f862c1593a5cd0f1682b
Preston Bernstein <prestonbernstein@gmail.com>
2014-04-15 17:49:11 -0400

    Class_12 is finished

--- class_12/db/schema.rb
+++ class_12/db/schema.rb
@@ -14,6 +14,8 @@
 ActiveRecord::Schema.define(version: 20140415181318) do
 
   create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
