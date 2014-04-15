ec665a03032f6e6ca0e376daa36aeefd53c471d8
Preston Bernstein <prestonbernstein@gmail.com>
2014-04-14 21:19:13 -0400

    Blog debug

--- class_11/config.ru
+++ class_11/config.ru
@@ -1,2 +1,4 @@
+#This is a config file that is used by a lot of deployment/tools. (tux, Heroku, shotgun)
+
 require './app'
 run Sinatra::Application
--- class_9/website/server.rb
+++ class_9/website/server.rb
@@ -1,6 +1,22 @@
-require "sinatra"
+require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(
+  :default,
+  'mysql://root@localhost/blog'
+  )
+
+class Post
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :blog_post, String
+end
+
+DataMapper.finalize.auto_upgrade!
 
 get '/' do
+  @posts = Post.all
 	erb :index, layout: :index
 end
 
@@ -10,4 +26,10 @@ end
 
 get '/newpost' do
   erb :newpost, layout: :newpost
+end
+
+post '/newpost' do
+  @post = Post.create params[:blog]
+  
+  redirect to('/')
 end
