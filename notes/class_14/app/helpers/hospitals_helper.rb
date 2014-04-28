354e65ab6ba01129d0624a45a392a5dbca323c1c
Preston Bernstein <prestonbernstein@gmail.com>
2014-04-27 22:42:13 -0400

    GOT THE PATIENT LIST TO DISPLAYgit add -Aadd

--- notes/class_14/app/helpers/hospitals_helper.rb
+++ notes/class_14/app/helpers/hospitals_helper.rb
@@ -1,2 +1,7 @@
 module HospitalsHelper
+  def workflow_links(patient)
+    if patient.workflow_state == 'waiting'
+      x = link_to "Doctor", doctor_hospital_patient_path(@hospital, patient), method: :patch
+    end
+  end
 end
