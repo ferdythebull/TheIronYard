a6568df5777637177f4d01929b81d3731deef809
Preston Bernstein <prestonbernstein@gmail.com>
2014-04-27 23:27:50 -0400

    added helper method

--- notes/class_14/app/helpers/hospitals_helper.rb
+++ notes/class_14/app/helpers/hospitals_helper.rb
@@ -1,7 +1,24 @@
 module HospitalsHelper
   def workflow_links(patient)
     if patient.workflow_state == 'waiting'
-      x = link_to "Doctor", doctor_hospital_patient_path(@hospital, patient), method: :patch
+      a = link_to "Doctor", doctor_hospital_patient_path(@hospital, patient), method: :patch
+      a += link_to "Xray", xray_hospital_patient_path(@hospital,patient), method: :patch
+      a += link_to "Surgery", surgery_hospital_patient_path(@hospital, patient), method: :patch
+      a += link_to "Leaving", leaving_hospital_patient_path(@hospital, patient), method: :patch
+    elsif patient.workflow_state == 'doctor'
+      s = link_to "Xray", xray_hospital_patient_path(@hospital, patient), method: :patch
+      s += link_to "Surgery", surgery_hospital_patient_path(@hospital, patient), method: :patch
+      s += link_to "Pay", pay_hospital_patient_path(@hospital, patient), method: :patch
+    elsif patient.workflow_state == 'xray'
+      d = link_to "Doctor", doctor_hospital_patient_path(@hospital, patient), method: :patch
+      d += link_to "Surgery", surgery_hospital_patient_path(@hospital, patient), method: :patch
+      d += link_to "Pay", pay_hospital_patient_path(@hospital, patient), method: :patch
+    elsif patient.workflow_state == 'surgery'
+      f = link_to "Doctor", doctor_hospital_patient_path(@hospital, patient), method: :patch
+      f += link_to "Xray", xray_hospital_patient_path(@hospital, patient), method: :patch
+      f += link_to "Pay", pay_hospital_patient_path(@hospital, patient), method: :patch
+    elsif patient.workflow_state == 'pay'
+      link_to "Check Out", leaving_hospital_patient_path(@hospital, patient), method: :patch
     end
   end
 end
