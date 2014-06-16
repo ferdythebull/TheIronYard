5cbe5bf8d86a7add7ff551fd89ec25c77f19fcb1
Preston Bernstein <prestonbernstein@gmail.com>
2014-06-16 17:05:17 -0400

    asdfsda

--- portfolio/petslist/db/migrate/20140614223019_remove_locations_join_tables.rb
+++
@@ -1,7 +0,0 @@
-class RemoveLocationsJoinTables < ActiveRecord::Migration
-  def change
-    drop_table :animal_locations
-    drop_table :shelter_locations
-    drop_table :user_locations
-  end
-end
